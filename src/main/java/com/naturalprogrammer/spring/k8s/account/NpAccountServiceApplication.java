6f8c0bfaa14dd5ddb0884c8f096756ce28ca2b41
Sanjay Patel <skpatel20@gmail.com>
2023-06-13 10:47:33 +0530

    Project generated via Spring Initializer

---
+++ src/main/java/com/naturalprogrammer/spring/k8s/account/NpAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.spring.k8s.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naturalprogrammer/spring/k8s/account/NpAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.spring.k8s.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
